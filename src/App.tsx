f41075517fd52e0d65b444652f20a99b8718a0ff
ValentinZoia <valentinzoia@gmail.com>
2024-10-22 19:04:30 -0300

    feat: add fetching function to get repositories

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+
+import { useFetchRepositories } from "./hooks/useRepos";
+
 
 export default function App() {
+
+  const {data, isLoading, error} = useFetchRepositories()
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>{error.message}</div>
+  }
+
+  
+
   return (
-    <div className="text-3xl font-bold text-red-800 underline p-5 bg-blue-200">
-      App
+    <div className="p-2 grid grid-cols-3 gap-4">
+      {data?.map(repo =>(
+        <div key={repo.id} className="flex flex-col bg-slate-700 p-4 rounded-lg">
+          <h1 className="text-2xl text-slate-200">{repo.name}</h1>
+          
+          <p className="text-slate-200">{repo.description}</p>
+          <p className="text-blue-400">{repo.language}</p>
+          <p className="text-slate-200">{new Date(repo.created_at).toLocaleDateString()}</p>
+          <p className={repo.private ? "text-red-500" : "text-green-500"}>{repo.private ? 'Privado' : 'Publico'}</p>
+        </div> 
+       ))}
+      
     </div>
   );
 }
---
+++ src/hooks/useRepos.ts
@@ -0,0 +1,22 @@
+import api from "../api/github";
+import { useQuery } from "@tanstack/react-query";
+import { ReposType } from "../models/Repos";
+
+async function getRepos() {
+  try {
+    const { data } = await api.get<ReposType[]>("users/ValentinZoia/repos");
+    return data;
+  } catch (error) {
+    throw new Error("Error fetching repos");
+  }
+}
+
+
+export function useFetchRepositories(){
+    return useQuery<ReposType[]>({
+        queryKey: ["repos"],
+        queryFn: getRepos
+    });
+
+    
+}
